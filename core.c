73d217bdf4d3bfe3d264d63b14cd45df98ce0023
BahriCan <47694291+BahriCan@users.noreply.github.com>
2021-09-21 11:28:50 -0400

    Add files via upload

--- core.c
+++ core.c
@@ -3,10 +3,12 @@
 
 int main()
 {
+    // a new version, we'll be using fgets() instead of scanf() now. i see that it is dangerous...
     int monster_alpha;
     int player_health;
     int punch_attack_power;
-    char character_name[255];
+    char character_name[255]; // SHIIIT IF I DO NOT PROVIDE A VALUE IT'LL NEVER BE BUFFER OVERRUN FUCK YEAAH
+    // never mind.. i have to provide a value for single-dimensional arrays. OR I COULD JUST = TO FGETS... nah not gonna happen :/
     char special_character[1] = "[";
     // eh fuck it I'll just use an integer variable to define true or false values. 0-false, 1-true.
     int goblin_is_defeated = 0;
@@ -17,22 +19,27 @@ int main()
 
     printf("Welcome to the alpha version of <insert game name here>.\nCOPYRIGHT u/Bahrican798.\n");
     printf("I know, it's not much, obviously. This is as much as I can get done in this environment...\n");
-    char user_input[100];
+    char user_input[255];
     printf("===================================================\n");
     printf("\t\t TYPE \"Start\" TO PLAY\t\t\t\n");
     printf("===================================================\n");
     do
     {
         printf(">||: ");
-        scanf("%s", user_input);    // try fgets maybe... I'm tired. the if statements dont work... skips directly to else and returns 2.
+        fgets(user_input,255,stdin);    // try fgets maybe... I'm tired. the if statements dont work... skips directly to else and returns 2.
+        user_input[strcspn(user_input, "\n")] = 0;
+        //printf("%s",user_input); okay fgets works fine... although strcmp is not happy with how I compare strings now that I use fgets it seems.
         // printf("you wrote: %s", user_input); // although when checked in with printf, it seems that scanf reads everything, not just the first character only... I guess it does not store it as a variable then?
+        // FIXED IT :D the "\n" is important in fgets apparently, never forget that!!! never forget the \n!!! hold the fuck up something's wrong here... the ".n" is printed onto a newline? this shit could prove to be a problem in the future, better solve it now. Alright it's fixed! learned how to get rid of the attached newline on the string!
         if(strcmp(user_input, "Start") == 0) // FUCK YES FINALLY IT CAN START
         {
             isInvalidInput = 0; // do-while condition is set to false, hence ending the loop and achieving what we did with goto. no more spaghetti code that's hard to track, yaaay!!.. it's fucking, what, 2am? im tired..
             // phew... almost lost all progress there, LiveOSes are a dangerous environment to do development on!
             printf("Choose your name, choose wisely for it can NOT be changed later on! Max. 255 characters: ");
-            scanf("%s", character_name);
-            printf("Your name is %s\n", character_name);
+            fgets(character_name,255,stdin); // why does fgets not work? does it have something to do with the loop? the if statement bothers it maybe? fuck im tired
+            // yeah replaced the scanf here and everywhere else with fgets, I have seen that it crashes if the user inputs an overflowing value.. and there is nothing to stop the user from doing that :P
+            character_name[strcspn(character_name, "\n")] = 0;
+            printf("Your name is %s.n", character_name);
             printf("Welcome... to a new world.\n");
             printf("You're wondering around.\n");
             printf("You keep walking for hours on end.\n");
@@ -42,8 +49,11 @@ int main()
              // well making a string variable and putting '[' in it and printing it via printf %s IS an option... but not efficient. There MUST be a way to just print those characters without a hassle!
              // I gotta get some work done on this...
              // I better put it into the "to be done" section, I usually don't read comments.
-            scanf("%s", user_input);
-            if (strcmp(user_input, "Y") == 0 || strcmp(user_input, " ") == 0) // fuck how do I detect if the user just pressed enter, do I just not let them?
+            fgets(user_input,255,stdin);
+            user_input[strcspn(user_input, "\n")] = 0;
+            //printf("%s.n", user_input);
+            // okay we either use fgets and chop off the attached newline each time, which is a bother, or just include in the newline when comparing strings and only chop off the newline when needed to which'll result in fewer lines of code. latter sounds better, right?
+            if (strcmp(user_input, "Y") == 0 || user_input[0] == '\0') // fuck how do I detect if the user just pressed enter, do I just not let them?
             {
                 printf("debug is working fine\n");
 
@@ -83,9 +93,10 @@ int main()
             printf("-Give the user a limited inventory, like in uhh.. umm.... i dont fucking remember the game name just give the user a small inventory and items around them to pick up/drop.\n");
             printf("-Either use command line options like --start, --debug and whatever, or use an in-game prompt like we are now. u/Urthemando_Mod looks like he is thirsty for cli options, so...\n");
             printf("-Perhaps make the variables an array? IF we ever add difficulty options to this, that could be useful to load in different values, e.g 10 damage for a monster in easy mode, 15 in normal, 20 in expert, 300/200/150 player health etc.\n");
-            printf("-Check fgets, scanf seems to be a pain...\n");
+            //printf("-Check fgets, scanf seems to be a pain...\n");
             printf("Added/fixed stuff:\n");
-            printf("+Finally stopped using goto, I am using for loops instead now that for stopped being such a dick and started working...");
+            printf("+Finally stopped using goto, I am using for loops instead now that for stopped being such a dick and started working...\n");
+            printf("+Got rid of scanf, using fgets now.\n");
             printf("This is all for now I guess. New ideas come by every now and then, which I'll note down here in this 'Easter Egg' :P.\n");
             return 1;
 
