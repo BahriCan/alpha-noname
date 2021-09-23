a5ed100bbf70bc835cff2f026fc99045fd1fc9b3
BahriCan <47694291+BahriCan@users.noreply.github.com>
2021-09-23 08:07:05 -0400

    Add files via upload

--- core.c
+++ core.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 #define MAX_LEN 128
+// ahh much better now that the code is reformatted
 
 void print_image(FILE * imagefile);
 
@@ -13,9 +14,11 @@ int main()
     int punch_attack_power;
     char character_name[255]; // SHIIIT IF I DO NOT PROVIDE A VALUE IT'LL NEVER BE BUFFER OVERRUN FUCK YEAAH
     // never mind.. i have to provide a value for single-dimensional arrays. OR I COULD JUST = TO FGETS... nah not gonna happen :/
-    char special_character[2] = "[";
     // eh fuck it I'll just use an integer variable to define true or false values. 0-false, 1-true.
     int goblin_is_defeated = 1;
+    int left_path_wood_is_taken = 0; // I consider zero to be false, 1 to be true. The woods in the left path are initially not taken, they will be once the player goes to the left path.
+    int right_path_wood_is_taken = 0;
+    int straight_path_wood_is_taken = 0;
     int isInvalidInput = 1; // this variable is to detect whether or not the user entered a correct value, it'll turn into a 0 and be considered false, and end the loop and continue off of that IF/ELSE block.
     monster_alpha = 15;
     player_health = 200;
@@ -43,50 +46,96 @@ int main()
             fgets(character_name,255,stdin); // why does fgets not work? does it have something to do with the loop? the if statement bothers it maybe? fuck im tired
             // yeah replaced the scanf here and everywhere else with fgets, I have seen that it crashes if the user inputs an overflowing value.. and there is nothing to stop the user from doing that :P
             character_name[strcspn(character_name, "\n")] = 0;
-            printf("Your name is %s.n", character_name);
+            printf("Your name is %s.", character_name);
+            sleep(1);
             printf("Welcome... to a new world.\n");
+            sleep(1.5);
             printf("You're wondering around.\n");
+            sleep(1.5);
             printf("You keep walking for hours on end.\n");
+            sleep(3);
             printf("It's getting late...\n");
             isInvalidInput = 1;
-            do{
-            printf("You stumble upon a little cave. Go inside? Y/n >||: ");
-            // I need to find a way to print [], (), {}....
-             // well making a string variable and putting '[' in it and printing it via printf %s IS an option... but not efficient. There MUST be a way to just print those characters without a hassle!
-             // I gotta get some work done on this...
-             // I better put it into the "to be done" section, I usually don't read comments.
-            fgets(user_input,255,stdin);
-            //printf("%s.n", user_input);
-            // okay we either use fgets and chop off the attached newline each time, which is a bother, or just include in the newline when comparing strings and only chop off the newline when needed to which'll result in fewer lines of code. latter sounds better, right?
-            if (strcmp(user_input, "Y\n") == 0 || user_input[1] == '\0') // fuck how do I detect if the user just pressed enter, do I just not let them? okay nevermind figured that part out
+            do
             {
-                isInvalidInput = 0;
-                char * filename = "image.txt";
-                FILE * imagefile = NULL;
-
-                if((imagefile = fopen(filename,"r")) == NULL)
+                printf("You stumble upon a little cave. Go inside? Y/n >||: ");
+                // I need to find a way to print [], (), {}....
+                // well making a string variable and putting '[' in it and printing it via printf %s IS an option... but not efficient. There MUST be a way to just print those characters without a hassle!
+                // I gotta get some work done on this...
+                // I better put it into the "to be done" section, I usually don't read comments.
+                fgets(user_input,255,stdin);
+                //printf("%s.n", user_input);
+                // okay we either use fgets and chop off the attached newline each time, which is a bother, or just include in the newline when comparing strings and only chop off the newline when needed to which'll result in fewer lines of code. latter sounds better, right?
+                if (strcmp(user_input, "Y\n") == 0 || user_input[1] == '\0') // fuck how do I detect if the user just pressed enter, do I just not let them? okay nevermind figured that part out
                 {
-                    fprintf(stderr,"error opening %s\n",filename);
-                    return 1;
+                    isInvalidInput = 0;
+                    char * filename = "image.txt";
+                    FILE * imagefile = NULL;
+
+                    if((imagefile = fopen(filename,"r")) == NULL)
+                    {
+                        fprintf(stderr,"error opening %s\n",filename);
+                        return 1;
+                    }
+
+                    print_image(imagefile);
+
+                    fclose(imagefile);
+                    sleep(3);
+                    printf("\nYou feel a cold breeze. You think of starting a campfire, and go out.");
+
                 }
+                else if (strcmp(user_input, "N\n") == 0 || strcmp(user_input, "n\n") == 0) // I could use strncmp to make the check case-insensitive, which would shorten the code.. but do I really wanna use strncmp? looks like a bother to me
+                {
 
-                print_image(imagefile);
+                    isInvalidInput = 0;
+                    printf("You decided not to go inside the cave.\n");
+                    sleep(3);
+                    printf("It's getting cold... You imagine a warm campfire, and set out to get some firewood.\n");
+                    printf(".\n");
+                    sleep(1);
+                    printf("..\n");
+                    sleep(1);
+                    printf("...\n");
+                    sleep(5);
+                    printf("You wander in the forest, searching for some wood.\n");
+                    sleep(0.5);
+                    printf("You have three paths ahead of you.\n");
+                    sleep(0.5);
+the_threeway_path_in_the_woods:
+                    do
+                    {
+                        printf("Go [L]eft,[R]ight,[S]traight. >||: ");
+                        fgets(user_input, 255, stdin);
+                        if(strcmp(user_input, "L") == 0 || strcmp(user_input, "l") == 0)
+                        {
+                            isInvalidInput = 0;
+                            printf("\nYou chose to go left.\n");
+                            // now to write a loop that'll take the user here again if they wanna, and check if the wood here has been already taken.
 
-                fclose(imagefile);
-                sleep(3);
-                printf("\nYou feel a cold breeze. You think of starting a campfire, and go out.");
+                            // printf("Go back?(Y/n)>||: ");
+                            //FUUCK I LOVE DOING THIS SO MUCH, I LOVE PROGRAMMING I'M JUST _FLOWING_ WITH IDEAS RIGHT NOW!!♥♥♥
+                            //fgets(user_input, 255, stdin);
+                            //ahh no wait.. should I really use goto here? I mean, I can write comments to explain why and where exactly it is heading, and I can refactor it later on if it starts getting messy with goto and replace it with do-while...
+                            //if(strcmp(user_input, "Y\n") == 0 || strcmp(user_input, "y\n") == 0 || user_input[1] == '\0')
+                            //{
+                            //}
+                        }
+                        else if(strcmp(user_input, "R") == 0 || strcmp(user_input, "r") == 0)
+                        {
+                            printf("\nYou chose to go right.\n");
 
+                        }
+                        else if(strcmp(user_input, "S") == 0 || strcmp(user_input, "s") == 0)
+                        {
+                            printf("\nYou chose to go straight ahead.\n");
+                        }
+                    }
+                    while(isInvalidInput);
+
+                }
             }
-            else if (strcmp(user_input, "N\n") == 0 || strcmp(user_input, "n\n") == 0) // I could use strncmp to make the check case-insensitive, which would shorten the code.. but do I really wanna use strncmp? looks like a bother to me
-            {
-                isInvalidInput = 0;
-                printf("You decided not to go inside the cave.\n");
-                sleep(3);
-                printf("It's getting cold... You imagine a warm campfire, and set out to get some firewood.");
-
-            }
-            }while(isInvalidInput);
-            make_a_campfire:
+            while(isInvalidInput);
             return 0;
         }
 
@@ -103,7 +152,7 @@ int main()
             printf("-Small creatures on the road, and the option to fight them or leave them alone. I'll try to use boolean to determine if they are defeated or not, which then will block the user from killing the same creature again and again for free EXP and HP. Now that I mention this.. a new idea popped in, check the next one for it.\n");
             printf("-Be able to go back the routes you take... I'm not sure how I'll do that, I'll figure it out. But not today.\n");
             printf("-Earn money, exp etc. from killed creatures.\n");
-            printf("-Have a stamina that increases on a timer. I don't know how I'll implement that in a console game, I'll keep reading the book to see if there is something like waiting a few seconds, otherwise I'll set up a loop with while or for, and calculate the time that passes by, and increase the stamina variable that way.\n");
+            printf("-Have a stamina that increases on a timer. I don't know how I'll implement that in a console game, I'll keep reading the book to see if there is something like waiting a few seconds, otherwise I'll set up a loop with while or for, and calculate the time that passes by, and increase the stamina variable that way. We can do this with a function and the sleep command! sleep for e.g 5 seconds, mana += 20!!\n");
             printf("-Be able to learn new abilites.. Reminds me of M&L: Superstar Saga\n");
             printf("-Settings? Maybe? what would one want to change in this \"game\" anyway\n");
             printf("-The player will be able to see how much exp left to level up... I'm still considering this, it's possible but why should I let them?\n");
@@ -117,7 +166,7 @@ int main()
             printf("-Add an option to save the game... I don't know how to do that, I'll do it but not today.\n");
             printf("- Make a function in a separate file, prototype this first in Python though. Some sort of a loop, a while loop, that'll take different values like how for or printf does. This loop will be used for fights. It'll be a barebones function, we'll fill in the blanks--- string to print out like 'You defeated the boss!', 'You used punch, you did 20 damage'--- ourselves.\n");
             printf("-Randomize monsters' attacks. They'll have various attacks that do different amounts of damage. Use a random number generator or something ;_;\n");
-            printf("Find a way to printf the following character, it's in the source code. Here I'll even print it here, BUT this is a troublesome way to print out a '%s'.\n", special_character);
+            //printf("Find a way to printf the following character, it's in the source code. Here I'll even print it here, BUT this is a troublesome way to print out a '%s'.\n", special_character);
             //printf("- DONT use goto! Apparently it's bad practice... find an alternative. Should be worth it, especially when you look at the old CS source code.. Damn that sure was one helluva spaghetti code.\n");
             printf("-Give the user a limited inventory, like in uhh.. umm.... i dont fucking remember the game name just give the user a small inventory and items around them to pick up/drop.\n");
             printf("-Either use command line options like --start, --debug and whatever, or use an in-game prompt like we are now. u/Urthemando_Mod looks like he is thirsty for cli options, so...\n");
@@ -129,6 +178,7 @@ int main()
             printf("Added/fixed stuff:\n");
             printf("+Finally stopped using goto, I am using for loops instead now that for stopped being such a dick and started working...\n");
             printf("+Got rid of scanf, using fgets now.\n");
+            printf("+Added sleep so that it waits between printing stuff!\n");
             printf("This is all for now I guess. New ideas come by every now and then, which I'll note down here in this 'Easter Egg' :P.\n");
             return 1;
 
@@ -137,11 +187,11 @@ int main()
         {
             isInvalidInput = 0; // ends the do-while loop by setting the loop condition to false
             for( ; monster_alpha > 0;) // I FUCKING HATE THE FOR STATEMENT IN C WHY DO I HAVE TO "iNItiaLiZE" SHIT I JUST WANNA GIVE A SINGLE CONDITION AND BE FUCKING DONE WITH IT
-            // Oh nevermind ._. apparently I can just put in a semicolon and fuck off with it... why didn't work the first time I tried it though? I did the exact same thing..
-            // this is pain
-            // hold on, just a few more lines and we can release the pre-alpha version
-            // alpha-release note, I could potentially replace this for loop with a do-while loop. i dont know which is more efficient...
-            // do-while seems more explanotary, i mean i had a fucking heart-stroke while trying to understand this loop i wrote..
+                // Oh nevermind ._. apparently I can just put in a semicolon and fuck off with it... why didn't work the first time I tried it though? I did the exact same thing..
+                // this is pain
+                // hold on, just a few more lines and we can release the pre-alpha version
+                // alpha-release note, I could potentially replace this for loop with a do-while loop. i dont know which is more efficient...
+                // do-while seems more explanotary, i mean i had a fucking heart-stroke while trying to understand this loop i wrote..
             {
                 printf("test: ");
                 scanf("%s", user_input);
@@ -162,7 +212,8 @@ int main()
             //printf("I couldn't write something to send the user back to the prompt, sorry.."); // Well I COULD use goto... but this thing is about to evolve into a few thousand lines of code, doesn't feel right to use goto
             //^ NOT TRUE ANYMORE!! Finally figured out how to set up that do-while loop!! :DD so happy... wonder if u/Urthemando_Mod will be pleased with this "progress"... probs not, still. i finally got rid of the burden that is goto.
         }
-    } while(isInvalidInput);
+    }
+    while(isInvalidInput);
     return 0;
 
 }
@@ -174,4 +225,3 @@ void print_image(FILE * imagefile)
     while(fgets(read_string,sizeof(read_string),imagefile) != NULL)
         printf("%s",read_string);
 }
-
