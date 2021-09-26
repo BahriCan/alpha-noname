c7d3934e8f109c690e1191495df85fb30097e0c6
badcodewriter <47694291+BahriCan@users.noreply.github.com>
2021-09-26 07:32:55 -0400

    Add files via upload

--- core.c
+++ core.c
@@ -2,19 +2,34 @@
 #include <string.h>
 #include <unistd.h>
 #define MAX_LEN 128
+// We can use this typedef struct for monsters and the player(s), it'll especially be useful if we ever put in a multiplayer mode.
+// Tomorrow morning we'll be doing a cleanup now that the isInvalidInput variable has been deprecated and the user prompt is a function now..
 // ahh much better now that the code is reformatted
-
+char user_input[255]; // now to refactor the code...
 void print_image(FILE * imagefile);
-int user_prompt();
+int user_prompt()
+{
+
+    printf(">||: ");
+    fgets(user_input,255,stdin); // oh yeah... only for user_input, not character_name
+}
+//now.. what were we doing? oh yeah.. the user stat function. and structures... i think that's where we left off
+typedef struct{
+    char * name;
+    int health_points;
+    int defense_points;
+    int attack_points;
+    int mana_points;
+}basic_character_structure;
 
 int main() // add in command-line options with int argc, char *argv[]... it'll need a refactoring
 {
     // a new version, we'll be using fgets() instead of scanf() now. i see that it is dangerous...
-    int monster_alpha;
-    int player_health;
-    int player_defense;
-    int punch_attack_power;
-    char character_name[255]; // SHIIIT IF I DO NOT PROVIDE A VALUE IT'LL NEVER BE BUFFER OVERRUN FUCK YEAAH
+    //int monster_alpha;
+    //int player_health;
+    //int player_defense;
+    //int punch_attack_power; oh yeah.. CHECK IN WITH THIS VARIABLE, WE MIGHT HAVE TO CREATE A SPECIAL STRUCTURE FOR THE PLAYER THEMSELVES AND THE MONSTERS SEPARATELY
+    //char character_name[255]; // SHIIIT IF I DO NOT PROVIDE A VALUE IT'LL NEVER BE BUFFER OVERRUN FUCK YEAAH
     // never mind.. i have to provide a value for single-dimensional arrays. OR I COULD JUST = TO FGETS... nah not gonna happen :/
     // eh fuck it I'll just use an integer variable to define true or false values. 0-false, 1-true.
     int goblin_is_defeated = 1;
@@ -22,38 +37,47 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
     int right_path_wood_is_taken = 0;
     int straight_path_wood_is_taken = 0;
     int user_wood_amount = 0;
-    int isInvalidInput = 1; // this variable is to detect whether or not the user entered a correct value, it'll turn into a 0 and be considered false, and end the loop and continue off of that IF/ELSE block.
+    //int isInvalidInput = 1; // this variable is to detect whether or not the user entered a correct value, it'll turn into a 0 and be considered false, and end the loop and continue off of that IF/ELSE block.
+    // now to deprecate isInvalidInput
     //int isInvalidInput2 = 1; // another variable bcuz i cant use the same one in nested do-while loops/// DEPRECATED VARIABLE, VARIABLES NO LONGER NEEDED FOR DO-WHILE LOOPS
-    monster_alpha = 15;
-    player_health = 200;
-    player_defense = 10;
-    punch_attack_power = 5;
+    //monster_alpha = 15;
+    //player_health = 200;
+    //player_defense = 10;
+    //punch_attack_power = 5;
+    // man I said a clean-up... not more messy, deprecated, commented out code!!
+    basic_character_structure player;
+    player.attack_points = 10;
+    player.defense_points = 20;
+    player.health_points = 200;
+    player.mana_points = 30;
+    player.name;
+    // yeah player.name comes in after the game starts and the user chooses a name.
 
     printf("Welcome to the alpha version of <insert game name here>.\nCOPYRIGHT u/Bahrican798.\n"); // I gotta find a name, man... Zagros maybe? does a game with that name exist?
     printf("I know, it's not much, obviously. This is as much as I can get done in this environment...\n");
     printf("It is recommended that you play this game in fullscreen.\n");
-    char user_input[255];
+   // char user_input[255];
     printf("===================================================\n");
     printf("\t\t<insert game name here>\t\t\n");
     printf("\t\t TYPE \"Start\" TO PLAY\t\t\t\n");
     printf("===================================================\n");
     do
     {
-        printf(">||: ");
-        fgets(user_input,255,stdin);    // try fgets maybe... I'm tired. the if statements dont work... skips directly to else and returns 2.
+        user_prompt();// try fgets maybe... I'm tired. the if statements dont work... skips directly to else and returns 2.
         user_input[strcspn(user_input, "\n")] = 0;
         //printf("%s",user_input); okay fgets works fine... although strcmp is not happy with how I compare strings now that I use fgets it seems.
         // printf("you wrote: %s", user_input); // although when checked in with printf, it seems that scanf reads everything, not just the first character only... I guess it does not store it as a variable then?
         // FIXED IT :D the "\n" is important in fgets apparently, never forget that!!! never forget the \n!!! hold the fuck up something's wrong here... the ".n" is printed onto a newline? this shit could prove to be a problem in the future, better solve it now. Alright it's fixed! learned how to get rid of the attached newline on the string!
         if(strcmp(user_input,"Start") == 0 || strcmp(user_input, "start") == 0)// FUCK YES FINALLY IT CAN START
         {
-            isInvalidInput = 0; // do-while condition is set to false, hence ending the loop and achieving what we did with goto. no more spaghetti code that's hard to track, yaaay!!.. it's fucking, what, 2am? im tired..
+            // do-while condition is set to false, hence ending the loop and achieving what we did with goto. no more spaghetti code that's hard to track, yaaay!!.. it's fucking, what, 2am? im tired..
             // phew... almost lost all progress there, LiveOSes are a dangerous environment to do development on!
-            printf("Choose your name, choose wisely for it can NOT be changed later on! Max. 255 characters: ");
-            fgets(character_name,255,stdin); // why does fgets not work? does it have something to do with the loop? the if statement bothers it maybe? fuck im tired
+            printf("Choose your name, choose wisely for it can NOT be changed later on! Max. 255 characters >||: ");
+            fgets(player.name,255,stdin); // why does fgets not work? does it have something to do with the loop? the if statement bothers it maybe? fuck im tired
             // yeah replaced the scanf here and everywhere else with fgets, I have seen that it crashes if the user inputs an overflowing value.. and there is nothing to stop the user from doing that :P
-            character_name[strcspn(character_name, "\n")] = 0;
-            printf("Your name is %s.", character_name);
+            // we could perhaps make this user_prompt() function receive arguments and replace the fgets(user_input,255,stdin) with something else with those arguments..
+            player.name[strcspn(player.name, "\n")] = 0; // i hope this works...
+            printf("Your name is %s.", player.name);
             sleep(1);
             printf(" Welcome... to a new world.\n");
             sleep(1.5);
@@ -62,20 +86,20 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
             printf("You keep walking for hours on end.\n");
             sleep(3);
             printf("It's getting late...\n");
-            isInvalidInput = 1;
-            do
+//            isInvalidInput = 1;
+            do // im so sleepy lloll ..... apparently there is alternatives to goto.. stupid fucking book telling me to use goto...
             {
-                printf("You stumble upon a little cave. Go inside? [Y/n] >||: ");
+                printf("You stumble upon a little cave. Go inside? [Y/n] ");
+                user_prompt();
                 // I need to find a way to print [], (), {}....
                 // well making a string variable and putting '[' in it and printing it via printf %s IS an option... but not efficient. There MUST be a way to just print those characters without a hassle!
                 // I gotta get some work done on this...
                 // I better put it into the "to be done" section, I usually don't read comments.
-                fgets(user_input,255,stdin);
                 //printf("%s.n", user_input);
                 // okay we either use fgets and chop off the attached newline each time, which is a bother, or just include in the newline when comparing strings and only chop off the newline when needed to which'll result in fewer lines of code. latter sounds better, right?
                 if (strcmp(user_input, "Y\n") == 0 || strcmp(user_input, "y\n") == 0 || user_input[1] == '\0') // fuck how do I detect if the user just pressed enter, do I just not let them? okay nevermind figured that part out
                 {
-                    isInvalidInput = 0;
+//                    isInvalidInput = 0;
                     char * filename = "mountains.txt";
                     FILE * imagefile = NULL;
 
@@ -111,7 +135,7 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
                 else if (strcmp(user_input, "N\n") == 0 || strcmp(user_input, "n\n") == 0) // I could use strncmp to make the check case-insensitive, which would shorten the code.. but do I really wanna use strncmp? looks like a bother to me
                 {
 
-                    isInvalidInput = 0;
+//                    isInvalidInput = 0;
                     printf("You decided not to go inside the cave.\n");
                     sleep(3);
                     printf("It's getting cold... You imagine a warm campfire, and set out to get some firewood.\n");
@@ -125,14 +149,14 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
                     sleep(0.5);
                     printf("You have three paths ahead of you.\n");
                     sleep(0.5);
-                    do
+                    do // the threeway path loop, Left, Right, Straight.
                     {
-                        printf("Go [L]eft,[R]ight,[S]traight, [G]o back. >||: ");
-                        fgets(user_input, 255, stdin);
+                        printf("Go [L]eft,[R]ight,[S]traight, [G]o back. ");
+                        user_prompt();
                         //stupid me, I forgot the \n in the user string :DDD such a relief, it is better than an orgasm
                         if(strcmp(user_input, "L\n") == 0 || strcmp(user_input, "l\n") == 0)
                         {
-                            isInvalidInput = 0;
+//                            isInvalidInput = 0;
                             printf("\nYou chose to go left.\n");
                             if(left_path_wood_is_taken == 0)
                             {
@@ -214,7 +238,7 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
                 }
                 break;
             }
-            while(isInvalidInput);
+            while(1);
             // printf("\nhere's where you make a campfire");
             printf("\nYou return to the cave.\n");
             sleep(3);
@@ -231,7 +255,24 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
                 printf("...\n");
                 sleep(1);
                 printf("Your defense dropped by 5. You got a cold and your back hurts because you sleeped on the cold, hard ground.\n");// i can either make a boolean variable, check this in later outside of the campfire scene, or print it out inside here...
-                player_defense -= 5; // I will forgive the player if they decide to do a morning run and warm up. (player_defense += 5;)
+                player.defense_points -= 5; // I will forgive the player if they decide to do a morning run and warm up. (player_defense += 5;)
+                printf("Do a morning run and stretches to warm up? [Y/n]  ");
+                user_prompt();
+                if(strcmp(user_input, "Y\n") == 0 || strcmp(user_input, "y\n") == 0 || user_input[1] == '\0')
+                {
+                    printf("You start running around in the cave, while trying not to get lost.\n");
+                    sleep(5);
+                    printf("You get tired. You stop before you start sweating.\n");
+                    sleep(3);
+                    printf("You gained 2 defense points. You're still sore from sleeping on the ground, and your nose is runny.\n");
+                    player.defense_points += 2; // good thinking with the player_* variable naming! it made it real easy to replace!!
+                    sleep(4);
+                }
+                else if(strcmp(user_input, "N\n") == 0 || strcmp(user_input,"n\n") == 0)
+                {
+                    printf("You decide to just look at the task at hand.\n");
+                    sleep(3);
+                }
             }
             if(user_wood_amount > 2 && user_wood_amount <= 5)
             {
@@ -275,9 +316,11 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
                 sleep(2);
                 printf("...\n");
                 sleep(5);
-                player_defense += 5;
+                player.defense_points += 5;
+                printf("You wake up.\n");
                 printf("\nYour defense has increased by 5! You did morning stretches and had a good sleep.\n");
             }
+
             printf("\n\tSorry, folks. This is the demo for now! Check in later for progression and stay tuned!\n");
             return 0;
         }
@@ -323,12 +366,14 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
             printf("+Finally stopped using goto, I am using for loops instead now that for stopped being such a dick and started working...\n");
             printf("+Got rid of scanf, using fgets now.\n");
             printf("+Added sleep so that it waits between printing stuff!\n");
+            printf("+Replaced every prompt with the new user_prompt() function\n");
             printf("This is all for now I guess. New ideas come by every now and then, which I'll note down here in this 'Easter Egg' :P.\n");
 
         }
+
         else if(strcmp(user_input, "test") == 0)
         {
-            isInvalidInput = 0; // ends the do-while loop by setting the loop condition to false
+            /*isInvalidInput = 0; // ends the do-while loop by setting the loop condition to false
             for( ; monster_alpha > 0;) // I FUCKING HATE THE FOR STATEMENT IN C WHY DO I HAVE TO "iNItiaLiZE" SHIT I JUST WANNA GIVE A SINGLE CONDITION AND BE FUCKING DONE WITH IT
                 // Oh nevermind ._. apparently I can just put in a semicolon and fuck off with it... why didn't work the first time I tried it though? I did the exact same thing..
                 // this is pain
@@ -346,7 +391,40 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
                 }
 
             }
+*/
+            /*user_prompt();
+            if(strcmp(user_input, "tes\n") == 0)
+            {
+                printf("\nSuccess!");
+            }*/
 
+            user_prompt();
+            if(strcmp(user_input, "print\ goblin\n") == 0) // ugh i hate the space... why do i always forget the \n ? XD
+            {
+                basic_character_structure goblin;
+                goblin.attack_points = 10;
+                goblin.defense_points = 5;
+                goblin.health_points = 25;
+                goblin.name = "Goblin";
+                printf("\nEnemy name: %s\n", goblin.name);
+                printf("Enemy max. health: %d\n", goblin.health_points);
+                printf("Enemy Attack points: %d\n", goblin.attack_points);
+                printf("Enemy Defense Points: %d\n", goblin.defense_points);
+                return 0;
+            }
+            else if (strcmp(user_input, "print\ player\n") == 0)
+            {
+                basic_character_structure player;
+                player.name = "John";
+                player.attack_points = 10;
+                player.health_points = 100;
+                player.defense_points = 20;
+                printf("\nPlayer name: %s\n", player.name);
+                printf("Player AP: %d\n", player.attack_points);
+                printf("Player DP: %d\n", player.defense_points);
+                printf("Player HP: %d\n", player.health_points);
+                return 0;
+            } // O- key!! the structs are preeety much finished... this'll make my stuff easier. I just gotta add the mana in, and I'm done for now with the basic character structure I guess. I'll just do a few corrections and deprecations here and there... deprecate the current player variables and replace them with the struct ones, do corrections inside the game (player_defense -= 5; X player.defense_points -= 5; !)
             // Here's an idea.. don't erase the stuff you put in this chunk of code. Instead just comment them out, or put in goto statements and special commands which'll allow you to jump among testing grounds
         }
         else if (strcmp(user_input, "demo") == 0)
@@ -422,7 +500,7 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
             printf("\nThink your way through fights harder than the existential crisis you have at 3AM strategically! Every choice you make affects the game!\n");
             sleep(0.4);
             printf("Challenge yourself in <insert game name here>!\n");
-            isInvalidInput = 1;
+//            isInvalidInput = 1;
         }
         else if(strcmp(user_input, "exit") == 0)
         {
@@ -437,7 +515,7 @@ int main() // add in command-line options with int argc, char *argv[]... it'll n
             //^ NOT TRUE ANYMORE!! Finally figured out how to set up that do-while loop!! :DD so happy... wonder if u/Urthemando_Mod will be pleased with this "progress"... probs not, still. i finally got rid of the burden that is goto.
         }
     }
-    while(isInvalidInput);
+    while(1);
     return 0;
 
 }
