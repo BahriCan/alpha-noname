2ee14f66c83bd88f3a10f676965c5814dccc360e
badcodewriter <47694291+BahriCan@users.noreply.github.com>
2021-12-31 20:20:06 +0300

    Update core.c

--- core.c
+++ core.c
@@ -1,4 +1,19 @@
 // TEST RELEASE
+/*******************************************************
+
+* Copyright (C) 2019-2022 BahriCan 
+*
+
+* This file is part of alpha-noname.
+
+*
+
+* alpha-noname can not be copied and/or distributed.
+
+*******************************************************/
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
